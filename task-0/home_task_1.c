2842cd85a714609aa0a3d628c4be08816e2a94dc
root <root@Alex-deb.Alex-deb>
2015-03-05 00:17:38 +0300

    last updates

--- task-0/home_task_1.c
+++ task-0/home_task_1.c
@@ -20,6 +20,7 @@ FILE * initFile(char * file_name)
             printf("FILE ERROR");
             exit(1);
    }
+   return fp;
 }
 int * create_shuffle(const int length)
 {
@@ -37,64 +38,65 @@ int * create_shuffle(const int length)
     }
     return Arr;
 }
-float InitLine(int * Array, const int LENGTH,int * Values)
+float InitLine(int * Array, const int length)
 {
-    int timeInit = clock();
-    int i,j;
-    for(i = 0; i< LENGTH; ++i)
+    long timeInit = clock();
+    int i;
+    long summ = 0;
+    for(i = 0; i< length; ++i)
     {
-        Array[i] = Values[i];
+        summ += Array[i];
     }
-    free(Values);
-   return (float)(clock() - timeInit)/ (CLOCKS_PER_SEC);
+   return (clock() - timeInit)/ (float)(CLOCKS_PER_SEC);
 }
-float InitLineM(const int M, int * Array, const int LENGTH,int * Values)
+float InitLineM(const int M, int * Array, const int length)
 {
     int start = clock();
-    int i,j;
+    int i,j=0;
+    long summ=0;
     for(j = 0;j<M;++j)
     {
-        for(i = j;i<LENGTH;i+= M)
+        for(i = j;i<length;i+= M)
         {
-            Array[i] = Values[i];
+            summ += Array[i];
         }
     }
-    free(Values);
-    return (float)(clock() - start)/ (CLOCKS_PER_SEC);
+    return (clock() - start)/ (float)(CLOCKS_PER_SEC);
 }
-float InitLineRand(int * Arr,int * Shuffle, int LENGTH)
+float InitLineRand(int * Arr,int * Shuffle, const int length)
 {
     int i;
-    int start = clock();
-    for(i = 0;i<LENGTH;++i)
+    long start = clock();
+    long summ = 0;
+    for(i = 0;i<length;++i)
     {
-        Arr[Shuffle[i]] = Shuffle[i];
+        summ += Arr[Shuffle[i]];
     }
     free(Shuffle);
-    return (float)(clock() - start)/ (CLOCKS_PER_SEC);
+    return (clock() - start)/ (float)(CLOCKS_PER_SEC);
 }
 int main(int argc,char ** argv)
 {
-    int LENGTH; /*LENGTH OF ARAY*/
+    int length; /*LENGTH OF ARAY*/
     int * Array;
-    int M; // param to gallop
-    int startTime;
+    int M; /*param to gallop*/
     float timeIn[3];
+    FILE * fp;
     if(argc != 4)
     {
         printf("error_number of arguments");
         exit(1);
     }
-    FILE * fp = initFile(argv[1]);
-    LENGTH = atoi(argv[2]);
+    fp = initFile(argv[1]);
+    length = atoi(argv[2]);
     M = atoi(argv[3]);
     srand(time(NULL)); /* update random()*/
-    startTime = clock();
-    assert(Array = (int*)malloc(sizeof(int)*LENGTH));
-    timeIn[0] = InitLine(Array,LENGTH,create_shuffle(LENGTH));
-    timeIn[1] = InitLineM(M,Array,LENGTH,create_shuffle(LENGTH));
-    timeIn[2] = InitLineRand(Array,create_shuffle(LENGTH),LENGTH);
-    fprintf(fp,"%d;%d;%.3f;%.3f;%.3f;\n",LENGTH,M,timeIn[0],timeIn[1],timeIn[2]);
+    assert(Array = (int*)malloc(sizeof(int)*length));
+    Array = create_shuffle(length);
+    timeIn[0] = InitLine(Array,length);
+    timeIn[1] = InitLineM(M,Array,length);
+    timeIn[2] = InitLineRand(Array,create_shuffle(length),length);
+    fprintf(fp,"%d;%d;%.3f;%.3f;%.3f;\n",length,M,timeIn[0],timeIn[1],timeIn[2]);
     free(Array);
     fclose(fp);
     return 0;
--- task-0/readme.txt
+++ task-0/readme.txt
@@ -1,1 +1,9 @@
 This is readme for task 0
+home_task_1.c - основная программа
+compile.sh - скрипт для компилирования с различными параметрами оптимизации
+script_length.sh - срипт тестирования L -длины
+script_step.sh - скрипт тестирования M - шага
+P.s: remake_script.sh (или remakecsv_script.sh) и remake_csvfile.c - доп. программы
+для изменения csv файла и замены в нем '.' на ','.(к делу не относится наверное) 
+
+
