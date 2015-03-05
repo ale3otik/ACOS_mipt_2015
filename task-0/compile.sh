35f495042cdd5492a04f4d870a6e5804904c6022
root <root@Alex-deb.Alex-deb>
2015-03-05 15:58:50 +0300

    new values for tests + cpu info

---
+++ task-0/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#compile
+gcc -ansi -pedantic -Wall -O0 home_task_1.c -o a_O0
+gcc -ansi -pedantic -Wall -O1 home_task_1.c -o a_O1
+gcc -ansi -pedantic -Wall -O2 home_task_1.c -o a_O2
+gcc -ansi -pedantic -Wall -O3 home_task_1.c -o a_O3
+gcc -ansi -pedantic -Wall -os home_task_1.c -o a_os
+#end of compile
