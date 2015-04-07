0c2ecbb61272a49c6366d84d728a0a7b25c38847
Alex <alex@MacBook-Pro-Alex.local>
2015-04-07 23:40:14 +0300

    modified first unlimited task environ

--- unlimited_tasks/environ.c
+++ unlimited_tasks/environ.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 /*
  * Он должен распечатать переметры программы, 
  * которые ей подавали на вход, и ещё 
  * массив кажется называется char **envp,
  * а __environ - это какой-то хак.
  */
-int main()
+int main(int argc, char* argv[], char* env[])
 {
     int i;
-    for(i=0;__environ [i] != NULL;++i)
+    
+    for (i = 0; i<argc; ++i)
     {
-        printf("%s\n" , __environ [i]);
-        ++i;
-    }	            
+        puts(*argv);
+    }
+    
+    puts("\nenviron:\n");
+    
+    while(*env)
+    {
+        puts(*env);
+        ++env;
+    }
     return 0;
 }
